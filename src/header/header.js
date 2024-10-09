b104ec56229786e55455fd05d8701de551b1295b
Lucas-Cordoba <cssmcordoba@gmail.com>
2024-10-09 16:26:04 -0300

    Lista la app con diseño responsivo para pantallas grandes y chicas

--- src/header/header.js
+++ src/header/header.js
@@ -6,22 +6,26 @@ const Header = () => {
     return (
         <header className='bg-light shadow p-3 mb-2 bg-white rounded sticky-top'>
             <div className="container">
-                <div className="row flex-lg-row">
-                    <div className="col-lg-4 mb-3 d-flex align-items-center justify-content-center">
-                        <h1 className="text-center">E-commerce DDS</h1>
+                <div className="row align-items-center">
+                    {/* Título */}
+                    <div className="col-lg-4 col-md-12 d-flex justify-content-center justify-content-lg-start mb-3">
+                        <h1 className="text-center text-lg-start">E-commerce DDS</h1>
                     </div>
-                    <div className="col-lg-4 col-6 mb-2 d-flex justify-content-center align-items-center">
-                        <nav>
+                    {/* Navegación y Carrito */}
+                    <div className="col-lg-4 col-md-12 d-flex justify-content-center justify-content-lg-end mb-3">
+                        <nav className="d-flex align-items-center">
                             <ul className="nav m-0">
-                                <li className="nav-item me-0 text-center">
-                                    <Link className="nav-link" to="/">Inicio</Link>
+                                <li className="nav-item me-2"> {/* Ajustar separación */}
+                                    <Link className="nav-link text-dark fw-bold fs-5 border border-dark rounded" to="/">Inicio</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <ContadorCarrito />
                                 </li>
                             </ul>
                         </nav>
                     </div>
-                    <div className="col-lg-4 col-6 mb-2 d-flex justify-content-center align-items-center">
-                        <ContadorCarrito className="ms-0" />
-                    </div>
+                    {/* Espacio vacío para la alineación correcta */}
+                    <div className="col-lg-4 d-none d-lg-flex justify-content-lg-end"></div>
                 </div>
             </div>
         </header>
