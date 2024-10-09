3b6f225f3278599b72c4d05cd9dec586e1b18c9b
Lucas-Cordoba <cssmcordoba@gmail.com>
2024-10-09 15:24:45 -0300

    cambios responsivos

--- src/header/header.js
+++ src/header/header.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ContadorCarrito from '../contadorCarrito/contadorCarrito'; // Asegúrate de que la ruta sea correcta
+import ContadorCarrito from '../contadorCarrito/contadorCarrito';
 
 const Header = () => {
     return (
-        <header className='bg-light shadow p-3 mb-5 bg-white rounded p-3 sticky-top'>
-            <div className="container d-flex justify-content-between align-items-center">
-                <h1 className="me-auto">E-commerce DDS</h1>
-                <nav className="d-flex align-items-center">
-                    <ul className="nav me-3">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/">Inicio</Link>
-                        </li>
-                    </ul>
-                    <div className="d-flex align-items-center ms-3"> {/* Añadido para margen */}
-                        <ContadorCarrito />
+        <header className='bg-light shadow p-3 mb-5 bg-white rounded sticky-top'>
+            <div className="container">
+                <div className="row flex-column flex-lg-row">
+                    <div className="col-lg-4 col-md-12 mb-3 d-flex align-items-center justify-content-center">
+                        <h1 className="text-center">E-commerce DDS</h1>
                     </div>
-                </nav>
+                    <div className="col-lg-4 col-md-12 mb-3 d-flex justify-content-end align-items-center">
+                        <nav>
+                            <ul className="nav m-0"> {/* Eliminar márgenes aquí */}
+                                <li className="nav-item me-0"> {/* Eliminar margen derecho */}
+                                    <Link className="nav-link" to="/">Inicio</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="col-lg-4 col-md-12 mb-3 d-flex justify-content-center align-items-center">
+                        <ContadorCarrito className="ms-0" /> {/* Asegúrate de que no tenga margen izquierdo */}
+                    </div>
+                </div>
             </div>
         </header>
     );
